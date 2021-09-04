f4337517815c7fff10a826fc58a581b1934fd8aa
DevinParentice <dude114100@gmail.com>
2021-09-03 21:50:58 -0400

    Set up providers

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/layout/App";
 
+import { ChakraProvider } from "@chakra-ui/react";
+import { AnimatePresence } from "framer-motion";
+
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ChakraProvider>
+            <AnimatePresence>
+                <App />
+            </AnimatePresence>
+        </ChakraProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
