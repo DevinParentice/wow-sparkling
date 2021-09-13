bc06fec6d9983f872e29bc47bd34aa8c9ef382c6
DevinParentice <dude114100@gmail.com>
2021-09-13 19:53:11 -0400

    Added stylelint configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
 /* eslint-disable */
 const autoprefixer = require("autoprefixer");
 const purgecss = require("@fullhuman/postcss-purgecss");
+const stylelint = require("stylelint");
 
 module.exports = {
     plugins: [
+        stylelint(),
         autoprefixer(),
         purgecss({
             content: ["./src/**/*.html", "./src/**/*.jsx", "./src/**/*.js"],
